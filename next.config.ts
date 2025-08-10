6cdb51c5ae07d863e4c476aa90e430e7f7b393dd
Amir-K <amirk_au@hotmail.com>
2025-08-10 13:06:14 -0700

    Make work (#5)
    * Removed minification when building to disable variable mangling
    * Local .next folder would not take into account new yalc update, so now it's being deleted before building

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,19 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-    webpack: (config, {isServer}) => {
-        if (isServer) {
-            config.devtool = 'source-map'
-        }
-        return config
-    },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+
+    config.optimization = {
+      ...config.optimization,
+      minimize: false,
+      minimizer: [],
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
