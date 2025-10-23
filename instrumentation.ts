7899b0a3957a689d60cdd7dddbe4dccfcefd44ba
Amir-K <amirk_au@hotmail.com>
2025-10-23 11:34:44 -0700

    local improvements

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,12 @@
-// export async function register() {
-//   console.log('In instrumentation');
-//   await new Promise((resolve) => setTimeout(resolve, 10000));
-// }
+export async function register() {
+  console.log('In instrumentation');
+  
+  // Handle SIGINT (Ctrl-C) gracefully
+  process.on('SIGINT', function() {
+    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+    // some other closing procedures go here
+    process.exit(0);
+  });
+  
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+}
